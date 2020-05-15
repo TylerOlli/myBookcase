90637872228ec856db9c0925270f7016b8551feb
Tyler Olli <ollityler@gmail.com>
2020-05-14 22:23:02 -0400

    feature: Setup CSSTransition on Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./App.css";
 import Shelf from "./Shelf";
-import Search from "./Search";
 
 class App extends React.Component {
   state = {
-    showSearchPage: false,
     books: [],
   };
 
@@ -33,49 +31,35 @@ class App extends React.Component {
   render() {
     const { books } = this.state;
     return (
-      <BrowserRouter>
-        <Route
-          exact
-          path='/'
-          render={() => (
-            <div className='app'>
-              <div className='list-books'>
-                <div className='list-books-title'>
-                  <h1>MyBookshelf</h1>
-                </div>
-                <div className='list-books-content'>
-                  <Shelf
-                    shelf={"currentlyReading"}
-                    books={books}
-                    onChangeShelf={this.changeShelf}
-                  />
-                  <Shelf
-                    shelf={"wantToRead"}
-                    books={books}
-                    onChangeShelf={this.changeShelf}
-                  />
-                  <Shelf
-                    shelf={"read"}
-                    books={books}
-                    onChangeShelf={this.changeShelf}
-                  />
-                </div>
-                <div className='open-search'>
-                  <Link to='/search'>
-                    <button>Add a book</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          )}
-        />
-        <Route
-          path='/search'
-          render={() => (
-            <Search onChangeShelf={this.changeShelf} books={books} />
-          )}
-        />
-      </BrowserRouter>
+      <div className='app'>
+        <div className='list-books'>
+          <div className='list-books-title'>
+            <h1>MyBookshelf</h1>
+          </div>
+          <div className='list-books-content'>
+            <Shelf
+              shelf={"currentlyReading"}
+              books={books}
+              onChangeShelf={this.changeShelf}
+            />
+            <Shelf
+              shelf={"wantToRead"}
+              books={books}
+              onChangeShelf={this.changeShelf}
+            />
+            <Shelf
+              shelf={"read"}
+              books={books}
+              onChangeShelf={this.changeShelf}
+            />
+          </div>
+          <div className='open-search'>
+            <Link to='/search'>
+              <button>Add a book</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Search from "./Search";
+import "./index.css";
+import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const AnimatedSwitch = withRouter(({ location }) => (
+  <TransitionGroup component={null}>
+    <CSSTransition
+      key={location.key}
+      classNames={location.key === 0 ? "pageSliderLeft" : "pageSliderRight"}
+      timeout={1000}
+    >
+      <div className='route__container'>
+        <Switch location={location}>
+          <Route path='/' component={App} exact />
+          <Route path='/search' component={Search} />
+        </Switch>
+      </div>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
+ReactDOM.render(
+  <BrowserRouter>
+    <AnimatedSwitch />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
