81c48a2ac9462e34781c077187484ca12ad5717a
Tyler Olli <ollityler@gmail.com>
2020-05-17 16:57:18 -0400

    feature: Fixed CSSTransitions and Updated Color Scheme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Switch,
-  withRouter,
-} from "react-router-dom";
+import { Route, Link, Switch, withRouter } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./App.css";
 import Shelf from "./Shelf";
@@ -38,62 +32,63 @@ class App extends React.Component {
 
   render() {
     const { books } = this.state;
-    const { history } = this.props;
+    const { location, history } = this.props;
     return (
-      <BrowserRouter>
-        <TransitionGroup component={null}>
-          <CSSTransition
-            key={history.location.key}
-            classNames={
-              history.location.pathname === "/"
-                ? "pageSliderLeft"
-                : "pageSliderRight"
-            }
-            timeout={1000}
-          >
-            <div className='route__container'>
-              <Switch history={history.location}>
-                <Route
-                  exact
-                  path='/'
-                  render={() => (
-                    <div className='app'>
-                      <div className='list-books'>
-                        <div className='list-books-title'>
-                          <h1>MyBookshelf</h1>
-                        </div>
-                        <div className='list-books-content'>
-                          <Shelf
-                            shelf={"currentlyReading"}
-                            books={books}
-                            onChangeShelf={this.changeShelf}
-                          />
-                          <Shelf
-                            shelf={"wantToRead"}
-                            books={books}
-                            onChangeShelf={this.changeShelf}
-                          />
-                          <Shelf
-                            shelf={"read"}
-                            books={books}
-                            onChangeShelf={this.changeShelf}
-                          />
-                        </div>
-                        <div className='open-search'>
-                          <Link to='/search'>
-                            <button>Add a book</button>
-                          </Link>
-                        </div>
-                      </div>
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          classNames={
+            history.location.pathname === "/"
+              ? "pageSliderLeft"
+              : "pageSliderRight"
+          }
+          timeout={1000}
+        >
+          <Switch history={history}>
+            <Route
+              exact
+              path='/'
+              render={() => (
+                <div className='app'>
+                  <div className='list-books'>
+                    <div className='list-books-title'>
+                      <h1>MyBookshelf</h1>
                     </div>
-                  )}
-                />
-                <Route path='/search' component={Search} />
-              </Switch>
-            </div>
-          </CSSTransition>
-        </TransitionGroup>
-      </BrowserRouter>
+                    <div className='list-books-content'>
+                      <Shelf
+                        shelf={"currentlyReading"}
+                        books={books}
+                        onChangeShelf={this.changeShelf}
+                      />
+                      <Shelf
+                        shelf={"wantToRead"}
+                        books={books}
+                        onChangeShelf={this.changeShelf}
+                      />
+                      <Shelf
+                        shelf={"read"}
+                        books={books}
+                        onChangeShelf={this.changeShelf}
+                      />
+                    </div>
+                    <div className='open-search'>
+                      <Link to='/search'>
+                        <button>Add a book</button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              )}
+            />
+            <Route
+              path='/search'
+              render={() => (
+                <Search onChangeShelf={this.changeShelf} books={books} />
+              )}
+            />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 }
