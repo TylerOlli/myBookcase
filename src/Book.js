a918ef439663d216847edbbc3542e9738bd3f47e
Tyler Olli <ollityler@gmail.com>
2020-05-25 22:09:26 -0400

    fix: Search works when no thumbnail or author

--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,7 @@ class Book extends React.Component {
                 style={{
                   width: 128,
                   height: 193,
-                  backgroundImage: `url(${book.imageLinks.thumbnail})`,
+                  backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ""})`,
                 }}
               />
               <div className='book-shelf-changer'>
